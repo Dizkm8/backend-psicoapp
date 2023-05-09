bfd29d7ec63428170fa7be8db9f10822c40b6fdb
David Araya <david.araya@alumnos.ucn.cl>
2023-05-09 12:39:22 -0400

    docs:Docs for program cs migrations

--- PsicoAppAPI/Program.cs
+++ PsicoAppAPI/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add database context with the configuration in appsettings.json
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -27,11 +29,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+// Seed the database with the json files if the database is empty.
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<DataContext>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 try
 {
+    // Migrate the database, create if it doesn't exist
     context.Database.Migrate(); 
     Seed.SeedData(context).Wait();
 }
