0bcb3f77bacfb7bb7828ce362e4c594ce81bdbf9
Jose Alcayaga <jose64am@aol.com>
2023-05-10 02:04:03 -0400

    AN-80 feat: created ForumController class

---
+++ PsicoAppAPI/Controllers/ForumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PsicoAppAPI.Data;
+namespace PsicoAppAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ForumController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ForumController(DataContext context) => _context = context;
+}
