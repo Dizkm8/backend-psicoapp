27e5ae896b1cfd2127bd1de59249b74042595aef
Jose Alcayaga <jose64am@aol.com>
2023-05-10 01:02:31 -0400

    AN-72 feat: added add feed post method

--- PsicoAppAPI/Controllers/FeedController.cs
+++ PsicoAppAPI/Controllers/FeedController.cs
@@ -11,4 +11,17 @@ public class FeedController : ControllerBase
     private readonly DataContext _context;
     
     public FeedController(DataContext context) => _context = context;
+    
+    /// <summary>
+    /// Add a feed post in database context 
+    /// </summary>
+    /// <param name="post">Post to add</param>
+    /// <returns>Post saved</returns>
+    [HttpPost]
+    public IActionResult AddFeedPost(FeedPost post)
+    {
+        _context.FeedPosts.Add(post);
+        _context.SaveChanges();
+        return Ok(post);
+    }
 }
