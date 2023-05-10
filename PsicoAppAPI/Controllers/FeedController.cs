19ed7db7586225d75a1b257d5856807c64932880
Jose Alcayaga <jose64am@aol.com>
2023-05-10 00:56:45 -0400

    AN-84 feat: created FeedController class

---
+++ PsicoAppAPI/Controllers/FeedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PsicoAppAPI.Data;
+using PsicoAppAPI.Models;
+namespace PsicoAppAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FeedController : ControllerBase
+{
+    private readonly DataContext _context;
+    
+    public FeedController(DataContext context) => _context = context;
+}
