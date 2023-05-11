1c2114edebcfa854943c86f2ba19a8b44f10481b
David Zeballos <david.zeballos@alumnos.ucn.cl>
2023-05-11 04:26:30 -0400

    Fixed controllers for them to work with Postman requests.

--- PsicoAppAPI/Models/FeedPost.cs
+++ PsicoAppAPI/Models/FeedPost.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+// TODO: [AN-112] Aplicar herencia para FeedPost y ForumPost (o fusionarlas)
 namespace PsicoAppAPI.Models
 {
     public class FeedPost
