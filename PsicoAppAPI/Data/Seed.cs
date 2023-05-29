fd0eb0afe6af4a3c4150b5485824b9bfb727bdf0
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-05-29 18:06:11 -0400

    AN-157

--- PsicoAppAPI/Data/Seed.cs
+++ PsicoAppAPI/Data/Seed.cs
@@ -18,6 +18,7 @@ namespace PsicoAppAPI.Data
             var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
             await SeedClients(context, options);
             await SeedSpecialities(context, options);
+            await SeedSpecialists(context, options);
             // await SeedFeedPosts(context, options);
             // await SeedForumPosts(context, options);
             // await SeedComments(context, options);
@@ -25,13 +26,35 @@ namespace PsicoAppAPI.Data
             await context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Seed the database with the specialists in the json file if the database is empty.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        /// <returns>Database adding task</returns>
+        private static async Task SeedSpecialists(DataContext context, JsonSerializerOptions options)
+        {
+            var result = context.Specialists?.Any();
+            if (result == true || result == null) return;
+            var specialistsData = File.ReadAllText("Data/Seeds/SpecialistsData.json");
+            var specialistsList = JsonSerializer.Deserialize<List<Specialist>>(specialistsData, options);
+            if (specialistsList == null) return;
+            await context.Specialists.AddRangeAsync(specialistsList);
+        }
+
+        /// <summary>
+        /// Seed the database with the specialities in the json file if the database is empty.
+        /// </summary>
+        /// <param name="context">Database context</param>
+        /// <param name="options">Options to deserialize json</param>
+        /// <returns>Database adding task</returns>
         private static async Task SeedSpecialities(DataContext context, JsonSerializerOptions options)
         {
             var result = context.Specialities?.Any();
             if (result == true || result == null) return;
             var specialitiesData = File.ReadAllText("Data/Seeds/SpecialitiesData.json");
             var specialitiesList = JsonSerializer.Deserialize<List<Speciality>>(specialitiesData, options);
-            if(specialitiesList == null) return;
+            if (specialitiesList == null) return;
             await context.Specialities.AddRangeAsync(specialitiesList);
         }
 
@@ -47,66 +70,10 @@ namespace PsicoAppAPI.Data
             if (result == true || result == null) return;
             var clientData = File.ReadAllText("Data/Seeds/ClientsData.json");
             var clientList = JsonSerializer.Deserialize<List<Client>>(clientData, options);
-            if(clientList == null) return;
+            if (clientList == null) return;
             await context.Clients.AddRangeAsync(clientList);
         }
 
-        // /// <summary>
-        // /// Seed the database with the feed posts in the json file if the database is empty.
-        // /// </summary>
-        // /// <param name="context"> Database Context </param>
-        // /// <param name="options"> Options to Deserialize json </param>
-        // /// <returns>Database adding Task</returns>
-        // private static async Task SeedFeedPosts(DataContext context, JsonSerializerOptions options)
-        // {
-        //     if (context.FeedPosts.Any()) return;
-        //     var feedPostsData = File.ReadAllText("Data/Seeds/FeedPostSeedData.json");
-        //     var feedPostsList = JsonSerializer.Deserialize<List<FeedPost>>(feedPostsData, options);
-        //     await context.FeedPosts.AddRangeAsync(feedPostsList);
-        // }
-
-        // /// <summary>
-        // /// Seed the database with the forum posts in the json file if the database is empty.
-        // /// </summary>
-        // /// <param name="context"> Database Context </param>
-        // /// <param name="options"> Options to Deserialize json </param>
-        // /// <returns>Database adding Task</returns>
-        // private static async Task SeedForumPosts(DataContext context, JsonSerializerOptions options)
-        // {
-        //     if (context.ForumPosts.Any()) return;
-        //     var forumPostsData = File.ReadAllText("Data/Seeds/ForumPostSeedData.json");
-        //     var forumPostsList = JsonSerializer.Deserialize<List<ForumPost>>(forumPostsData, options);
-        //     await context.ForumPosts.AddRangeAsync(forumPostsList);
-        // }
-
-        // /// <summary>
-        // /// Seed the database with the comments in the json file if the database is empty.
-        // /// </summary>
-        // /// <param name="context"> Database Context </param>
-        // /// <param name="options"> Options to Deserialize json </param>
-        // /// <returns>Database adding Task</returns>
-        // private static async Task SeedComments(DataContext context, JsonSerializerOptions options)
-        // {
-        //     if (context.Comments.Any()) return;
-        //     var commentsData = File.ReadAllText("Data/Seeds/CommentSeedData.json");
-        //     var commentsList = JsonSerializer.Deserialize<List<Comment>>(commentsData, options);
-        //     await context.Comments.AddRangeAsync(commentsList);
-        // }
-
-        // /// <summary>
-        // /// Seed the database with the appointments in the json file if the database is empty.
-        // /// </summary>
-        // /// <param name="context"> Database Context </param>
-        // /// <param name="options"> Options to Deserialize json </param>
-        // /// <returns>Database adding Task</returns>
-        // private static async Task SeedAppointments(DataContext context, JsonSerializerOptions options)
-        // {
-        //     if (context.Appointments.Any()) return;
-        //     var appointmentsData = File.ReadAllText("Data/Seeds/AppointmentSeedData.json");
-        //     var appointmentsList = JsonSerializer.Deserialize<List<Appointment>>(appointmentsData, options);
-        //     await context.Appointments.AddRangeAsync(appointmentsList);
-        // }
-       
-       #endregion
+        #endregion
     }
 }
