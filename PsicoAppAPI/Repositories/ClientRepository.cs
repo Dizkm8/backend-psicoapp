82887a518c42612a658938765d4f4b282e9d5663
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-05-29 20:53:28 -0400

    AN-168: Partial fix to DataContext to not accept nulls

--- PsicoAppAPI/Repositories/ClientRepository.cs
+++ PsicoAppAPI/Repositories/ClientRepository.cs
@@ -1,12 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PsicoAppAPI.Data;
+using PsicoAppAPI.Models;
+using PsicoAppAPI.Repositories.Interfaces;
 
 namespace PsicoAppAPI.Repositories
 {
-    public class ClientRepository
+    public class ClientRepository : IClientRepository
     {
-        
+        private readonly DataContext _context;
+         public ClientRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<bool> AddClientAndSaveChanges(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            var result = await _context.SaveChangesAsync() > 0;
+            return result;
+        }
+
+        public async Task<bool> ClientExists(string id)
+        {
+            var client = await _context.FindAsync<Client>(id);
+            return client != null;
+        }
+
+        public async Task<bool> ClientExists(Client client)
+        {
+            if(client.Id == null) return false;
+            var result = await this.ClientExists(client.Id);
+            return result;
+        }
+
+        public Task<Client?> GetClientByCredentials(string id, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client?> GetClientById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- PsicoAppAPI/Repositories/Interfaces/IClientRepository.cs
+++ PsicoAppAPI/Repositories/Interfaces/IClientRepository.cs
@@ -1,12 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PsicoAppAPI.Models;
 
 namespace PsicoAppAPI.Repositories.Interfaces
 {
     public interface IClientRepository
     {
-        
+        /// <summary>
+        /// Asynchronously get a client by their Id
+        /// </summary>
+        /// <param name="id">Client id</param>
+        /// <returns>The client if it's found, null if not</returns>
+        public Task<Client?> GetClientById(string id);
+
+        /// <summary>
+        /// Asynchronously get a client by their credentials 
+        /// </summary>
+        /// <param name="id">Client Id</param>
+        /// <param name="password">Client password</param>
+        /// <returns>The client if it's found, null if not</returns>
+        public Task<Client?> GetClientByCredentials(string id, string password);
+        /// <summary>
+        /// Asynchronous add a client to database and save the changes
+        /// </summary>
+        /// <param name="client">Client to add</param>
+        /// <returns>True if could be added</returns>
+        public Task<bool> AddClientAndSaveChanges(Client client);
+        /// <summary>
+        /// Save the changes in the database
+        /// </summary>
+        /// <returns>True if any change was made</returns>
+        public bool SaveChanges();
+        /// <summary>
+        /// Check if a client exists in the database
+        /// </summary>
+        /// <param name="id">Client id</param>
+        /// <returns>True if exists</returns>
+        public Task<bool> ClientExists(string id);
+        /// <summary>
+        /// Check if a client exists in the database by their Id
+        /// </summary>
+        /// <param name="client">Client to get Id</param>
+        /// <returns>True if exists</returns>
+        public Task<bool> ClientExists(Client client);
     }
 }
