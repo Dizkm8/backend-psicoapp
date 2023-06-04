44e6d542adce42e13d887f858c2366152081be36
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-06-03 21:49:21 -0400

    AN-242

--- PsicoAppAPI/Services/Interfaces/IAuthService.cs
+++ PsicoAppAPI/Services/Interfaces/IAuthService.cs
@@ -2,6 +2,22 @@ namespace PsicoAppAPI.Services.Interfaces
 {
     public interface IAuthService
     {
-        
+        /// <summary>
+        /// Generate a JWT token for the user
+        /// </summary>
+        /// <param name="id">User id to assign token</param>
+        /// <param name="userRole">User role to assign inside token</param>
+        /// <returns>string if the user id exists or wasn't null. Otherwise null</returns>
+        public string? GenerateToken(string? id, string? userRole);
+        /// <summary>
+        /// Get the user id from the token using HttpContext
+        /// </summary>
+        /// <returns>string with the Id. null if something gone wrong</returns>
+        public string? GetUserIdInToken();
+        /// <summary>
+        /// Get the user role from the token using HttpContext
+        /// </summary>
+        /// <returns>string with the Role. null if something gone wrong</returns>
+        public string? GetUserRoleInToken();
     }
 }
