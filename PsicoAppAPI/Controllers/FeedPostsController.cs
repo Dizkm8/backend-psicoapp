d4d62dd3d9f89cf6c22afbbe84112f3c8d1953e9
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-06-04 11:59:09 -0400

    AN-253

--- PsicoAppAPI/Controllers/FeedPostsController.cs
+++ PsicoAppAPI/Controllers/FeedPostsController.cs
@@ -20,8 +20,13 @@ namespace PsicoAppAPI.Controllers
         [HttpPost("create-post")]
         public async Task<ActionResult> AddFeedPost(AddFeedPostDto addFeedPost)
         {
+            var validateContent = await _service.CheckPostContext(addFeedPost);
+            if (!validateContent) return BadRequest(
+                new ErrorModel { ErrorCode = 400, Message = "The Content don't follow the rules to post" });
+
             var existsTag = await _service.CheckPostTag(addFeedPost);
             if (!existsTag) return NotFound($"Tag with ID {addFeedPost.TagId} does not exist");
+
             var postToReturn = await _service.AddFeedPost(addFeedPost);
             if (postToReturn is null) return StatusCode(StatusCodes.Status500InternalServerError,
                 new ErrorModel { ErrorCode = 500, Message = "Internal error creating feedpost" });
--- PsicoAppAPI/Services/Mediators/FeedPostManagementService.cs
+++ PsicoAppAPI/Services/Mediators/FeedPostManagementService.cs
@@ -11,15 +11,18 @@ namespace PsicoAppAPI.Services.Mediators
         private readonly IMapperService _mapperService;
         private readonly IUserService _userService;
         private readonly ITagService _tagService;
+        private readonly IOpenAIService _openAIService;
 
         public FeedPostManagementService(IFeedPostService feedPostService, IAuthService authService,
-            IMapperService mapperService, IUserService userService, ITagService tagService)
+            IMapperService mapperService, IUserService userService, ITagService tagService,
+            IOpenAIService openAIService)
         {
             _feedPostService = feedPostService;
             _authService = authService;
             _mapperService = mapperService;
             _userService = userService;
             _tagService = tagService;
+            _openAIService = openAIService;
         }
 
         public async Task<FeedPostDto?> AddFeedPost(AddFeedPostDto feedPostDto)
@@ -36,10 +39,18 @@ namespace PsicoAppAPI.Services.Mediators
             feedPost.PublishedOn = DateOnly.FromDateTime(DateTime.Now);
 
             var result = await _feedPostService.AddFeedPost(feedPost);
-            if(!result) return null;
+            if (!result) return null;
 
             var postDto = _mapperService.MapToFeedPostDto(feedPost);
             return postDto;
+        }
+
+        public async Task<bool> CheckPostContext(AddFeedPostDto feedPostDto)
+        {
+            var content = feedPostDto.Content;
+            if (content is null) return false;
+            var result = await _openAIService.CheckPostContent(content);
+            return result;
         }
 
         public async Task<bool> CheckPostTag(AddFeedPostDto feedPostDto)
