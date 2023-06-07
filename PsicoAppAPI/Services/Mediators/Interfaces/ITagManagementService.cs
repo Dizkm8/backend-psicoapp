23f28e5230c71c8b30ef83956b21294f163009d4
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-06-07 11:01:26 -0400

    AN-309

--- PsicoAppAPI/Services/Mediators/Interfaces/ITagManagementService.cs
+++ PsicoAppAPI/Services/Mediators/Interfaces/ITagManagementService.cs
@@ -1,7 +1,13 @@
+using PsicoAppAPI.DTOs.BasePosts;
+
 namespace PsicoAppAPI.Services.Mediators.Interfaces
 {
     public interface ITagManagementService
     {
-        
+        /// <summary>
+        /// Get all tags
+        /// </summary>
+        /// <returns>IEnumerable with tags</returns>
+        public Task<IEnumerable<TagDto>> GetTags();
     }
 }
--- PsicoAppAPI/Services/Mediators/TagManagementService.cs
+++ PsicoAppAPI/Services/Mediators/TagManagementService.cs
@@ -1,9 +1,22 @@
+using PsicoAppAPI.DTOs.BasePosts;
+using PsicoAppAPI.Services.Interfaces;
 using PsicoAppAPI.Services.Mediators.Interfaces;
 
 namespace PsicoAppAPI.Services.Mediators
 {
     public class TagManagementService : ITagManagementService
     {
+        private readonly ITagService _tagService;
 
+        public TagManagementService(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        public Task<IEnumerable<TagDto>> GetTags()
+        {
+            // var result = await _tagService.GetTags();
+            throw new NotImplementedException();
+        }
     }
 }
