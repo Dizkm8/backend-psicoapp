95f061b9e899a493d4353bc09bb21fd29df942e8
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-06-07 11:40:22 -0400

    AN-310

--- PsicoAppAPI/Controllers/TagsController.cs
+++ PsicoAppAPI/Controllers/TagsController.cs
@@ -1,7 +1,37 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PsicoAppAPI.Controllers.Base;
+using PsicoAppAPI.DTOs.BasePosts;
+using PsicoAppAPI.Services.Mediators.Interfaces;
+
 namespace PsicoAppAPI.Controllers
 {
-    public class TagsController
+    public class TagsController : BaseApiController
     {
-        
+        private readonly ITagManagementService _service;
+
+        public TagsController(ITagManagementService service)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        /// <summary>
+        /// Get all tags in the system shaped as TagDto
+        /// Needs to be logged in to avoid spam, all users can access this endpoint
+        /// </summary>
+        /// <returns>
+        /// Returns a list of TagDto, each tagDto have the following properties:
+        /// - Id: int
+        /// - Name: string
+        /// If the system have no tags, returns an empty list
+        /// If the user is not logged in, returns a 401 status: Unauthorized
+        /// </returns>
+        [Authorize(Roles = "1, 2, 3")]
+        [HttpGet]
+        public async Task<IEnumerable<TagDto>> GetTags()
+        {
+            var tags = await _service.GetTags();
+            return tags;
+        }
     }
 }
