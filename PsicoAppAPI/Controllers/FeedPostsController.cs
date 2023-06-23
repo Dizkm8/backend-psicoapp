3633995b253031bfed6c5fd39a33e1942b0be9d2
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2023-06-23 01:13:53 -0400

    AN-414 controller docs

--- PsicoAppAPI/Controllers/FeedPostsController.cs
+++ PsicoAppAPI/Controllers/FeedPostsController.cs
@@ -29,6 +29,7 @@ namespace PsicoAppAPI.Controllers
         /// If the user Id from the provided token doesn't match with a client return 500 Internal server error
         /// If something went wrong adding the post return 500 Internal server error
         /// If the tag id does not exist return 404 Not found
+        /// </returns>
         [Authorize(Roles = "3")]
         [HttpPost("create-post")]
         public async Task<ActionResult> AddFeedPost(AddFeedPostDto addFeedPost)
@@ -37,11 +38,35 @@ namespace PsicoAppAPI.Controllers
             if (!existsTag) return NotFound($"Tag with ID {addFeedPost.TagId} does not exist");
 
             var postToReturn = await _service.AddFeedPost(addFeedPost);
-            if (postToReturn is null) return StatusCode(StatusCodes.Status500InternalServerError,
-                new ErrorModel { ErrorCode = 500, Message = "Internal error creating feedpost" });
+            if (postToReturn is null)
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new ErrorModel { ErrorCode = 500, Message = "Internal error creating feedpost" });
             return Ok(postToReturn);
         }
-        
+
+        /// <summary>
+        /// Get all feed posts
+        /// </summary>
+        /// <returns>
+        /// If the user Id from the provided token doesn't match with a client return 500 Internal server error
+        /// If something went wrong fetching the posts return 500 Internal server error
+        /// If everything goes well, return a List with the following properties:
+        /// Id: Post's id
+        /// Title: Post's title
+        /// Content: Post's content
+        /// PublishedOn: Post's published date (ISO 8601)
+        /// UserId: Post's user id
+        /// 
+        /// The next three attributes are used to show the user's full name
+        /// I suggest threat like "private" stuff, so they are not used in the client side
+        /// use fullName attribute instead
+        /// UserName: Post's user name 
+        /// UserFirstLastName: Post's user first last name
+        /// UserSecondLastName: Post's user second last name
+        /// 
+        /// FullName: Post's user full name
+        /// TagName: Post's tag name
+        /// </returns>
         [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<FeedPostDto>>> GetAllForumPosts()
